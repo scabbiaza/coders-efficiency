7da5d8c9220080ed0c2e270e484f8d901e5acfe0
scabbiaza <scabbiaza@ya.ru>
2014-10-11 21:37:30 +0300

    Add basic template

--- codeff/__init__.py
+++ codeff/__init__.py
@@ -3,4 +3,8 @@ from flask import Flask
 
 app = Flask(__name__, static_folder=op.join(op.dirname(__dir__), 'static'))
 
+# JINJA ------------------------------------------------------------------------
+app.jinja_env.globals['locale'] = lambda: 'en'
+
+# VIEWS ------------------------------------------------------------------------
 import codeff.views
--- codeff/views.py
+++ codeff/views.py
@@ -1,24 +1,17 @@
-from codeff import app
-from codeff import stat
+from flask import render_template
+
+from codeff import app, stat
 
 
 @app.route('/')
-def index():
+def home():
     conf = {
         'repo_path': None,
-        'ignore': ['*.md', '2.py'],
+        'ignore': ['static/vendors/**', '2.py'],
         'min_days': 0,
         'min_loc': 0,
         'anonym': True,
     }
 
     team, authors = stat.get_authors_stat(conf)
-
-    print("{:<10}{:>20}{:>20}{:>20}{:>20}"
-          .format('Author', 'LOC total', 'Working days', 'Contribution, %', 'Efficiency'))
-    i = 0
-    for author_name, author in authors.iteritems():
-        i += 1
-        print("{:<10}{:>20}{:>20}{:>20.2%}{:>20.2}".format(author_name if not conf['anonym'] else 'Coder %d' % i, author['loc'], author['days'], author['contribution'], author['efficiency']))
-
-    return ''
+    return render_template('home.html', conf=conf, team=team, authors=authors)
