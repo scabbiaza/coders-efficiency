d518aad96e61da26f07565e35cf86b6053d12ae6
scabbiaza <scabbiaza@ya.ru>
2014-10-21 15:49:38 +0300

    Clear vendors
    Remove bower and build files form vendors.
    Let's keep the project simple as possible.

---
+++ static/dist/js/theme.js
@@ -0,0 +1,17 @@
+//$(document).ready(function () {
+//    $form = $('.ui.form.configurations');
+//    if ($form.length) {
+//        $form.form({
+//            inline: true,
+//            repo_path: {
+//                identifier: 'repo_path',
+//                rules: [
+//                    {
+//                        type: 'empty',
+//                        prompt: 'Field is required'
+//                    }
+//                ]
+//            }
+//        });
+//    }
+//});
